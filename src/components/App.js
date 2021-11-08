c7dfdbabbcb1a250eca149698a276a19228798b9
Kalpak <kalpakto26@gmail.com>
2021-11-09 01:15:10 +0530

    Basic structure created

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyPage from './MyPage';
+
+
+function App() {
+	return (
+		<MyPage></MyPage>
+	);
+}
+
+export default App;
---
+++ src/components/MyPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../style/MyPage.css'
+
+function MyPage() {
+	return (
+		<div className='landing-page'>
+			<div className="logo-container">
+				<div className="left-part"></div>
+				<div className="right-part">
+					<div className="main-stick"></div>
+					<div className="top-small-stick"></div>
+					<div className="bottom-small-stick"></div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default MyPage
