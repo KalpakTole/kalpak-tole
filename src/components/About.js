63f549dcbde4d6df56e4f7f620ae69acfefea624
Kalpak <kalpakto26@gmail.com>
2021-12-23 22:24:25 +0530

    Adding other description about myself

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,63 @@
 import React from "react";
 import "./About.css";
-import "../assets/IMG_9612.JPG";
+import profilepic from "../assets/IMG_9612.JPG";
 
 const About = () => {
 	return (
 		<div className="about-page">
 			<div className="left-side">
 				<img
-					src="http://localhost:8000/src/assets/IMG_9612.JPG"
+					src={profilepic}
 					alt="Profile Pic"
 					className="profile-photo"
 					// width="80%"
 					// height="90%"
 				/>
 			</div>
-			<div className="right-side"></div>
+			<div className="right-side">
+				<div className="name-plate">
+					<h1 className="myname">Kalpak Tole</h1>
+					<p className="mylocation">Mumbai, India</p>
+				</div>
+				<div className="about-myself">
+					<h3>
+						Just a usual teen trying to bring unusual changes to the
+						Web World...
+					</h3>
+					<details>
+						<summary>Work Experience</summary>
+						<h4>Systems Engineer</h4>
+						<h5>Tata Consultancy Services Ltd.</h5>
+						<span>08.2020 - PRESENT</span>
+						<span>MUMBAI, INDIA</span>
+						<ul>
+							<li>
+								Web development - Front-end (Angular and
+								Material UI) (August 2021 - Present)
+							</li>
+							<li>
+								AR-VR application development (April 2021 - July
+								2021)
+							</li>
+							<li>
+								Web development - Front-end (Python, Flask and
+								Bootstrap) (December 2020 - April 2021)
+							</li>
+							<li>
+								Meta Learning (September 2020 - December 2020)
+							</li>
+						</ul>
+					</details>
+				</div>
+				<div className="hobby-list">
+					<div className="ilike">I like </div>
+					<div className="hobbies">
+						<div className="webdev">Web Development</div>
+						<div className="illustrate">Illustrating</div>
+						<div className="python">Programming</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
